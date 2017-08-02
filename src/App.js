b09c97b7f632e550e72e1f5f5d6f5cd763084fbc
Ahsan Khan <ahsankhan1911@gmail.com>
2017-08-02 15:06:46 +0500

    helloo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Link} from 'react-router-dom'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
-
+// import Signedup from './Signedup'
 
 // const Links = () => (
 //   <nav>
@@ -12,15 +12,13 @@ import '../node_modules/bootstrap/dist/css/bootstrap.css'
 // )
 
 const NavRoutes =() => (
-   <nav className="navbar navbar-inverse">
+   <nav className="navbar navbar-inverse" style={{"border-radius": "0"}}>
   <div className="container-fluid">
     <div className="navbar-header">
       <a className="navbar-brand" >React Router App</a>
     </div>
     <ul className="nav navbar-nav">
       <li><Link to="/">Home</Link></li>
-       
-      <li><Link to="/about">About</Link> </li>
       <li><Link to="/contact">Contact</Link> </li>
       <li><Link to="/login">Log In</Link> </li>
       <li><Link to="/signup">Sign up</Link> </li>
@@ -34,22 +32,6 @@ const Home = () => (
   <NavRoutes /> 
 <h1>HELLO REACT</h1><img src={'./favicon.ico'} alt={"Ahsan"}/>
 </div>
-)
-
-const About = () => (
-  
-  <div>
-    <NavRoutes /> 
-    <div className="container-fluid">
-  <h2>About React</h2>
-  <div className="panel panel-default">
-    <div className="panel-body">JavaScript library. ... In computing, React (sometimes styled React.js or ReactJS) is an open-source JavaScript library for building user interfaces. It is maintained by Facebook, Instagram and a community of individual developers and corporations
-      React was created by Jordan Walke, a software engineer at Facebook. He was influenced by XHP, an HTML component framework for PHP.[8] It was first deployed on Facebook's newsfeed in 2011 and later on Instagram.com in 2012. It was open-sourced at JSConf US in May 2013. React Native, which enables native Android, iOS, and UWP development with React, was announced at Facebook's React.js Conf in February 2015 and open-sourced in March 2015. On April 18, 2017, Facebook announced React Fiber, a new core algorithm of React framework library for building user interfaces.[10] React Fiber will become the foundation of any future improvements and feature development of the React framework.
-    </div>
-    </div>
-  </div>
-</div>
-
 )
      
 const Contact = () => (
@@ -75,58 +57,65 @@ const Contact = () => (
 
 )
 
-
-
-
-
 class SignUp extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      value: '',
-      value2: ''};
+ constructor(props) {
+    super(props);
+    this.state = {input: '',
+                  input2: ''};
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-    handleChange(event) {
-    this.setState({value: event.target.value});
-  } 
+  handleChange(event) {
+    this.setState({input: this.refs.input.value,
+                   input2: this.refs.input2.value});
+  }
 
-    handleSubmit(event) {
-
-  <h1>Hellooooooo</h1>
-  
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
     event.preventDefault();
   }
 
-   render() {
-   return (
-     <div>
-              <form onSubmit={this.handleSubmit}>
-      
-       <div className="form-group">
-          <input type="text" value={this.state.value} className="form-control" onChange={this.handleChange} />
-    </div>
-    {/* <div className="form-group">
-          <input type="text" value={this.state.value} className="form-control" onChange={this.handleChange} />
-    </div> */}
+  render() {
+    return (
+      <div className="container">
+      <form onSubmit={this.handleSubmit}>
+        <div className="row form-group">
+          <input type="text" value={this.state.value} onChange={this.handleChange} className="form-control" ref="input"/>
+          </div>
+          <div className="row form-group">
+          <input type="text" value={this.state.value} onChange={this.handleChange} className="form-control" ref="input1"/>
+          </div>
+     
         <input type="submit" value="Submit" />
-        
       </form>
-   {console.log(this.props.children)}
-    </div>
-   );
+      </div>
+    );
+  }
 }
+
+class SignedUp extends React.Component {
+ 
+
+    render () {
+        return (
+            <div>
+                <h1>Yout account has been submitted</h1>
+            </div>
+        )
+    }
+
+
 }
+
+
 const App = () => (
  <Router>
 
    <div>
       {/* <Links/>  */}
      <Route exact path="/" component={Home}/>
-     <Route exact path="/about" component={About}/>
      <Route exact path="/contact" component={Contact}/>
      {/* <Route exact path="/login" component={LogIn}/> */}
       <Route exact path="/signup" component={SignUp}/> 
---
+++ src/Signup.js
@@ -0,0 +1,36 @@
+// import React from 'react'
+// import { BrowserRouter as Router, Route, Link} from 'react-router-dom'
+// import Signedup from './Signedup';
+
+
+
+// class SignUp extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//     value2: ''};
+//   }
+// onSubmit() {
+//     this.setState({
+//         value: this.state.value
+//     })
+// }
+ 
+
+//   render() {
+//     return (
+//         <div>
+//       <form onSubmit={() => this.onSubmit()}>
+//        <div className="form-group">
+//           <input className="form-control" type="text" value={this.state.value2} />
+//       </div>
+//         <input type="submit" value="Submit"/>
+//       </form>
+//       {this.state.value ? <Signedup/> : null}
+//   </div>
+//     );
+//   }
+// }
+
+
+// export default SignUp;
